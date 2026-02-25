29211772dd96aa82045418c532e4faeaedf1907c
aledifon <aledifon89@gmail.com>
2026-02-25 11:41:50 +0100

    Finished basics MVVM application with Add, Delete and Save Commands

--- MVVMApplication/MainWindow.xaml.cs
+++ MVVMApplication/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using MVVMApplication.ViewModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,8 @@ namespace MVVMApplication
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
         }
     }
 }
---
+++ MVVMApplication/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using MVVMApplication.Model;
+using MVVMApplication.MVVM;
+using System.Collections.ObjectModel;
+
+namespace MVVMApplication.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public RelayCommand AddCommand => new RelayCommand(execute => AddItem());
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteItem(), canExecute => SelectedItem != null);
+        public RelayCommand SaveCommand => new RelayCommand(execute => Save(), canExecute => canSave());
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+            //Items.Add(new Item
+            //{
+            //    Name = "Product1",
+            //    SerialNumber = "001",
+            //    Quantity = 10,
+            //});
+            //Items.Add(new Item
+            //{
+            //    Name = "Product2",
+            //    SerialNumber = "002",
+            //    Quantity = 20,
+            //});
+            //Items.Add(new Item
+            //{
+            //    Name = "Product3",
+            //    SerialNumber = "003",
+            //    Quantity = 6,
+            //});
+        }
+
+        private Item _selectedItem;
+        public Item SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();        // As we're using the Compiler Attribute CallerMemberName
+                                            // the 'SelectedItem' string will be passed automatically
+                                            // as parameter
+            }
+        }
+
+        private void AddItem()
+        {
+            Items.Add(new Item
+            {
+                Name = "NEW ITEM",
+                SerialNumber = "XXXX",
+                Quantity = 0,
+            });
+        }
+        private void DeleteItem()
+        {
+            Items.Remove(SelectedItem);
+        }
+
+        private void Save()
+        {
+            // Save to file/db
+        }
+
+        private bool canSave()
+        {
+            return true;
+        }
+    }
+}
