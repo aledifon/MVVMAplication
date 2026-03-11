d0e5b1888298e36b35920fbb661795e8dfb80434
aledifon <aledifon89@gmail.com>
2026-03-11 16:11:17 +0100

    Add Basic Create Methods to Insert new Data

--- MVVMApplication/Model/DBManager.cs
+++ MVVMApplication/Model/DBManager.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.Net;
 using System.Printing;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,122 +26,193 @@ namespace MVVMApplication.Model
         // GetAllOrdersByClient(ClientInfo)
         // GetAllArticlesByPriceRange(PriceRange)
 
-
-        #region Read Methods
-        public ObservableCollection<Article> GetAllArticles()
+        #region Check Methods
+        public bool FindClientByName(Client client)
         {
-            var lista = new ObservableCollection<Article>();
-
-            try
+            string query = "SELECT COUNT(*) " +
+                            $"FROM [{nameof(Client)}] " +
+                            $"WHERE [{nameof(Client.ClientName)}] = @ClientName";            
+            // Use of params to avoid SQL Injection
+            return ExecuteSqlNonReaderQuery(query, (cmd) =>
             {
-                using (var conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-
-                    string query = "SELECT * FROM " + nameof(Article);
-
-                    using (var cmd = new SqlCommand(query, conn))
-                    {
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                lista.Add(new Article
-                                {
-                                    Id = (int)reader[nameof(Article.Id)],
-                                    
-                                    Section = reader[nameof(Article.Section)] == DBNull.Value
-                                            ? null
-                                            : reader[nameof(Article.Section)].ToString(),
-                                    
-                                    ArticleName = reader[nameof(Article.ArticleName)] == DBNull.Value
-                                            ? null
-                                            : reader[nameof(Article.ArticleName)].ToString(),                                    
-
-                                    Price = reader[nameof(Article.Price)] == DBNull.Value
-                                            ? (decimal?)null
-                                            : (decimal)reader[nameof(Article.Price)],
-                                    
-                                    Date = reader[nameof(Article.Date)] == DBNull.Value
-                                            ? (DateTime?)null
-                                            : (DateTime)reader[nameof(Article.Date)],
-
-                                    OriginCountry = reader[nameof(Article.OriginCountry)] == DBNull.Value
-                                            ? null
-                                            : reader[nameof(Article.OriginCountry)].ToString(),
-                                });
-                            }
-                        }
-                    }
-                }
-            }
-            catch(SqlException ex)
-            {
-                Debug.WriteLine("Error en la consulta SQL: " + ex.Message,
-                    "Error de base de datos");
-            }
-
-            return lista;
+                cmd.Parameters.AddWithValue("@ClientName", client.ClientName);                
+                return (int)cmd.ExecuteScalar();
+            }) > 0;
         }
+        public bool FindArticleByName(Article article)
+        {
+            string query = "SELECT COUNT(*) " +
+                            $"FROM [{nameof(Article)}] " +
+                            $"WHERE [{nameof(Article.ArticleName)}] = @ArticleName";            
+            // Use of params to avoid SQL Injection
+            return ExecuteSqlNonReaderQuery(query, (cmd) =>
+            {
+                cmd.Parameters.AddWithValue("@ArticleName", article.ArticleName);                
+                return (int)cmd.ExecuteScalar();
+            }) > 0;
+        }
+        public bool FindClientByOrderId(int clientId)
+        {
+            string query = "SELECT COUNT(*) " +
+                            $"FROM [{nameof(Client)}] " +
+                            $"WHERE [{nameof(Client.Id)}] = @CClient";            
+            // Use of params to avoid SQL Injection
+            return ExecuteSqlNonReaderQuery(query, (cmd) =>
+            {
+                cmd.Parameters.AddWithValue("@CClient", clientId);                
+                return (int)cmd.ExecuteScalar();
+            }) > 0;
+        }
+        #endregion
+
+        #region Create Methods
+        public int AddNewClient(Client? newClient)
+        {
+            string query = $"INSERT INTO [{nameof(Client)}]" +
+                           $"([{nameof(Client.ClientName)}], [{nameof(Client.Address)}], " +
+                           $"[{nameof(Client.Location)}] , [{nameof(Client.Telephone)}])" +
+                           $"VALUES (@ClientName, @Address, @Location, @Telephone)";   
+            // Use of params to avoid SQL Injection
+            return ExecuteSqlNonReaderQuery(query, (cmd) =>
+            {
+                cmd.Parameters.AddWithValue("@ClientName", newClient.ClientName);
+                cmd.Parameters.AddWithValue("@Address", newClient.Address);
+                cmd.Parameters.AddWithValue("@Location", newClient.Location);
+                cmd.Parameters.AddWithValue("@Telephone", newClient.Telephone);
+
+                return cmd.ExecuteNonQuery(); 
+            });
+        }
+        public int AddNewOrder(Order? newOrder)
+        {
+            string query = $"INSERT INTO [{nameof(Order)}]" +
+                           $"([{nameof(Order.CClient)}], [{nameof(Order.DateOrder)}], " +
+                           $"[{nameof(Order.TypePayment)}])" +
+                           $"VALUES (@CClient, @DateOrder, @TypePayment)";
+            // Use of params to avoid SQL Injection
+            return ExecuteSqlNonReaderQuery(query, (cmd) =>
+            {
+                cmd.Parameters.AddWithValue("@CClient", newOrder.CClient);
+                cmd.Parameters.AddWithValue("@DateOrder", newOrder.DateOrder);
+                cmd.Parameters.AddWithValue("@TypePayment", newOrder.TypePayment);                
+
+                return cmd.ExecuteNonQuery(); 
+            });
+        }
+        public int AddNewArticle(Article? newArticle)
+        {
+            string query = $"INSERT INTO [{nameof(Article)}]" +
+                           $"([{nameof(Article.Section)}], [{nameof(Article.ArticleName)}], " +
+                           $"[{nameof(Article.Price)}], [{nameof(Article.Date)}]," +
+                           $"[{nameof(Article.OriginCountry)}])" +
+                           $"VALUES (@Section, @ArticleName, @Price, @Date, @OriginCountry)";
+            // Use of params to avoid SQL Injection
+            return ExecuteSqlNonReaderQuery(query, (cmd) =>
+            {
+                cmd.Parameters.AddWithValue("@Section", newArticle.Section);
+                cmd.Parameters.AddWithValue("@ArticleName", newArticle.ArticleName);
+                cmd.Parameters.AddWithValue("@Price", newArticle.Price);
+                cmd.Parameters.AddWithValue("@Date", newArticle.Date);
+                cmd.Parameters.AddWithValue("@OriginCountry", newArticle.OriginCountry);
+
+                return cmd.ExecuteNonQuery(); 
+            });
+        }
+        #endregion
+
+        #region Read Methods        
         public ObservableCollection<Client> GetAllClients()
         {
-            var lista = new ObservableCollection<Client>();
-
-            try
+            string query = "SELECT * FROM [" + nameof(Client) + "]";
+            return ExecuteSqlReaderQuery(query, reader => new Client
             {
-                // Using SqlDataReader
-                using (var conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
+                Id = (int)reader[nameof(Client.Id)],
 
-                    string query = "SELECT * FROM " + nameof(Client);
-
-                    using (var cmd = new SqlCommand(query, conn))
-                    {
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                lista.Add(new Client
-                                {
-                                    Id = (int)reader[nameof(Client.Id)],                                    
-
-                                    ClientName = reader[nameof(Client.ClientName)] == DBNull.Value
+                ClientName = reader[nameof(Client.ClientName)] == DBNull.Value
                                             ? null
                                             : reader[nameof(Client.ClientName)].ToString(),
 
-                                    Address = reader[nameof(Client.Address)] == DBNull.Value
+                Address = reader[nameof(Client.Address)] == DBNull.Value
                                             ? null
                                             : reader[nameof(Client.Address)].ToString(),
 
-                                    Location = reader[nameof(Client.Location)] == DBNull.Value
+                Location = reader[nameof(Client.Location)] == DBNull.Value
                                             ? null
                                             : reader[nameof(Client.Location)].ToString(),
 
-                                    Telephone = reader[nameof(Client.Telephone)] == DBNull.Value
+                Telephone = reader[nameof(Client.Telephone)] == DBNull.Value
                                             ? null
-                                            : reader[nameof(Client.Telephone)].ToString()                                    
-                                });
-                            }
+                                            : reader[nameof(Client.Telephone)].ToString()
+            });                        
+        }
+        public ObservableCollection<Order> GetAllOrders()
+        {
+            string query = "SELECT * FROM [" + nameof(Order) + "]";
+            return ExecuteSqlReaderQuery(query, reader => new Order
+            {
+                Id = (int)reader[nameof(Order.Id)],
+
+                CClient = (int)reader[nameof(Order.CClient)],
+
+                DateOrder = reader[nameof(Order.DateOrder)] == DBNull.Value
+                                                ? (DateTime?)null
+                                                : (DateTime)reader[nameof(Order.DateOrder)],
+
+                TypePayment = reader[nameof(Order.TypePayment)] == DBNull.Value
+                                                ? null
+                                                : reader[nameof(Order.TypePayment)].ToString()
+            });            
+        }
+        public ObservableCollection<Article> GetAllArticles()
+        {
+            string query = "SELECT * FROM [" + nameof(Article) + "]";
+            return ExecuteSqlReaderQuery(query, reader => new Article
+            {
+                Id = (int)reader[nameof(Article.Id)],
+
+                Section = reader[nameof(Article.Section)] == DBNull.Value
+                                        ? null
+                                        : reader[nameof(Article.Section)].ToString(),
+
+                ArticleName = reader[nameof(Article.ArticleName)] == DBNull.Value
+                                        ? null
+                                        : reader[nameof(Article.ArticleName)].ToString(),
+
+                Price = reader[nameof(Article.Price)] == DBNull.Value
+                                        ? (decimal?)null
+                                        : (decimal)reader[nameof(Article.Price)],
+
+                Date = reader[nameof(Article.Date)] == DBNull.Value
+                                        ? (DateTime?)null
+                                        : (DateTime)reader[nameof(Article.Date)],
+
+                OriginCountry = reader[nameof(Article.OriginCountry)] == DBNull.Value
+                                        ? null
+                                        : reader[nameof(Article.OriginCountry)].ToString(),
+            });
+        }
+        #endregion
+
+        #region Queries Methods
+        private ObservableCollection<T> ExecuteSqlReaderQuery<T>(string query, Func<SqlDataReader, T> mapper)
+        {
+            var lista = new ObservableCollection<T>();
+
+            try
+            {
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();                    
+
+                    using (var cmd = new SqlCommand(query, conn))                    
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lista.Add(mapper(reader));
                         }
-                    }
-                }
-
-                // Using SqlDataAdapter                
-                //using (var conn = new SqlConnection(connectionString))
-                //{
-                //    string query = "SELECT * FROM " + nameof(Client);
-
-                //    using (var myAdapterSql = new SqlDataAdapter(query,conn))
-                //    {
-                //        DataTable clientsTable = new DataTable();
-
-                //        myAdapterSql.Fill(clientsTable);
-                //    } 
-                //}
-
-                //// Afterwards will be needed to return a DataTable obj.
-                //// and iterating each row from it in order to get each Client Data.
+                    }                    
+                }             
             }
             catch (SqlException ex)
             {
@@ -150,50 +222,29 @@ namespace MVVMApplication.Model
 
             return lista;
         }
-        public ObservableCollection<Order> GetAllOrders()
+        private int ExecuteSqlNonReaderQuery(string query, Func<SqlCommand,int> mapper)
         {
-            var lista = new ObservableCollection<Order>();
+            int rowsAffected = -1;
 
             try
             {
                 using (var conn = new SqlConnection(connectionString))
                 {
-                    conn.Open();
+                    conn.Open();                    
 
-                    string query = "SELECT * FROM [" + nameof(Order) + "]";
-
-                    using (var cmd = new SqlCommand(query, conn))
-                    {
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                lista.Add(new Order
-                                {
-                                    Id = (int)reader[nameof(Order.Id)],
-
-                                    CClient = (int)reader[nameof(Order.CClient)],
-                                    
-                                    DateOrder = reader[nameof(Order.DateOrder)] == DBNull.Value 
-                                            ? (DateTime?)null 
-                                            : (DateTime)reader[nameof(Order.DateOrder)],
-
-                                    TypePayment = reader[nameof(Order.TypePayment)] == DBNull.Value
-                                            ? null
-                                            : reader[nameof(Order.TypePayment)].ToString()
-                                });
-                            }
-                        }
-                    }
-                }
+                    using (var cmd = new SqlCommand(query, conn))                                        
+                    {                       
+                        rowsAffected = mapper(cmd);
+                    }                    
+                }             
             }
-            catch(SqlException ex)
+            catch (SqlException ex)
             {
                 Debug.WriteLine("Error en la consulta SQL: " + ex.Message,
-                    "Error de base de datos");
+                    "Error de base de datos");                
             }
 
-            return lista;
+            return rowsAffected;
         }
         #endregion
     }
--- MVVMApplication/ViewModel/MainWindowViewModel.cs
+++ MVVMApplication/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using MVVMApplication.Model;
 using MVVMApplication.MVVM;
 using System.Collections.ObjectModel;
+using System.Net;
+using System.Windows;
 using System.Windows.Input;
 
 namespace MVVMApplication.ViewModel
@@ -10,11 +12,11 @@ namespace MVVMApplication.ViewModel
         // DB Manager Instance
         private DBManager _dbRepository;
 
-        // Properties used as binding with the View (UI)
-        public ObservableCollection<Client> Clients { get; set; }       // No need to call OnPropertyChanged for these props as
-                                                                        // ObservableCollection implements INotifyPropertyChanged
+        #region Properties
+        public ObservableCollection<Client> Clients { get; set; }       // No need to call OnPropertyChanged for these props as                                                                        // ObservableCollection implements INotifyPropertyChanged
         public ObservableCollection<Order> Orders { get; set; }       
         public ObservableCollection<Article> Articles { get; set; }     
+
         private Client? _selectedClient;
         public Client? SelectedClient
         {
@@ -27,6 +29,19 @@ namespace MVVMApplication.ViewModel
                                             // as parameter
             }
         }
+        private Client? _newClient;
+        public Client? NewClient
+        {
+            get { return _newClient; }
+            set
+            {
+                _newClient = value;
+                OnPropertyChanged();        // As we're using the Compiler Attribute CallerMemberName
+                                            // the 'SelectedItem' string will be passed automatically
+                                            // as parameter
+            }
+        }
+
         private Order? _selectedOrder;
         public Order? SelectedOrder
         {
@@ -39,6 +54,19 @@ namespace MVVMApplication.ViewModel
                                             // as parameter
             }
         }
+        private Order? _newOrder;
+        public Order? NewOrder
+        {
+            get { return _newOrder; }
+            set
+            {
+                _newOrder = value;
+                OnPropertyChanged();        // As we're using the Compiler Attribute CallerMemberName
+                                            // the 'SelectedItem' string will be passed automatically
+                                            // as parameter
+            }
+        }
+
         private Article? _selectedArticle;
         public Article? SelectedArticle
         {
@@ -51,27 +79,90 @@ namespace MVVMApplication.ViewModel
                                             // as parameter
             }
         }
-
-        // Commands       
-        public ICommand GetAllClientsCommand { get; }        
-        public ICommand GetAllOrdersCommand { get; }        
-        public ICommand GetAllArticlesCommand { get; }                          
+        private Article? _newArticle;
+        public Article? NewArticle
+        {
+            get { return _newArticle; }
+            set
+            {
+                _newArticle = value;
+                OnPropertyChanged();        // As we're using the Compiler Attribute CallerMemberName
+                                            // the 'SelectedItem' string will be passed automatically
+                                            // as parameter
+            }
+        }
+        #endregion 
 
         public MainWindowViewModel()
-        {            
-            _dbRepository = new DBManager();    
+        {
+            _dbRepository = new DBManager();
 
             // Create instances of every Command
             GetAllClientsCommand = new RelayCommand(GetAllClients);
             GetAllOrdersCommand = new RelayCommand(GetAllOrders, canExecute => true);
             GetAllArticlesCommand = new RelayCommand(GetAllArticles);
 
+            InsertClientCommand = new RelayCommand(AddNewClient);
+            InsertOrderCommand = new RelayCommand(AddNewOrder);
+            InsertArticleCommand = new RelayCommand(AddNewArticle);
+
             // Create instances of every collection
             Clients = new ObservableCollection<Client>();
             Orders = new ObservableCollection<Order>();
             Articles = new ObservableCollection<Article>();
-        }        
 
+            NewClient = new Client();
+            NewOrder = new Order();
+            NewArticle = new Article();
+        }
+
+        #region Commands
+        public ICommand GetAllClientsCommand { get; }        
+        public ICommand GetAllOrdersCommand { get; }        
+        public ICommand GetAllArticlesCommand { get; }
+
+        public ICommand InsertClientCommand { get; }
+        public ICommand InsertOrderCommand { get; }
+        public ICommand InsertArticleCommand { get; }
+        #endregion
+
+        #region Checking Methods
+        private bool FindClientByName(Client client) => _dbRepository.FindClientByName(client);
+        private bool FindArticleByName(Article article) => _dbRepository.FindArticleByName(article);
+        private bool FindClientByOrderId(int clientId) => _dbRepository.FindClientByOrderId(clientId);
+        #endregion
+
+        #region Data Validation Methods
+        private bool NewClientsEmptyFieldsValidation(Client client)
+        {
+            if (string.IsNullOrWhiteSpace(client.ClientName)) return false;
+            if (string.IsNullOrWhiteSpace(client.Address)) return false;
+            if (string.IsNullOrWhiteSpace(client.Location)) return false;
+            if (string.IsNullOrWhiteSpace(client.Telephone)) return false;
+
+            return true;
+        }
+        private bool NewArticlesEmptyFieldsValidation(Article article)
+        {
+            if (string.IsNullOrWhiteSpace(article.Section)) return false;
+            if (string.IsNullOrWhiteSpace(article.ArticleName)) return false;
+            if (article.Price <= 0) return false;
+            if (article.Date == null || article.Date > DateTime.Today) return false;
+            if (string.IsNullOrWhiteSpace(article.OriginCountry)) return false;
+
+            return true;
+        }
+        private bool NewOrderEmptyFieldsValidation(Order order)
+        {            
+            if (order.CClient <= 0) return false;
+            if (order.DateOrder == null || order.DateOrder > DateTime.Today) return false;
+            if (string.IsNullOrWhiteSpace(order.TypePayment)) return false;
+
+            return true;
+        }
+        #endregion
+
+        #region Read Methods
         private void GetAllClients(object? parameter)
         {
             var tempClients = _dbRepository.GetAllClients();
@@ -95,6 +186,109 @@ namespace MVVMApplication.ViewModel
             Articles.Clear();
             foreach (var c in tempArticles)
                 Articles.Add(c);
+        }
+        #endregion
+
+        #region Create Methods
+        private void AddNewClient(object? parameter)
+        {
+            // Data validation
+            if (NewClient == null)
+            {
+                MessageBox.Show($"Client data is missing");
+                return;
+            }                
+            else if (!NewClientsEmptyFieldsValidation(NewClient))
+            {
+                MessageBox.Show($"All Client fields are required.");
+                return;
+            }
+
+            // Format Data Validation (Optional)
+            // Max Length Validation (Optional)
+
+            // Duplicates validation
+            else if (FindClientByName(NewClient))
+            {
+                MessageBox.Show($"The inserted Client already exists on the DB.");
+                return;
+            }
+
+            // If all previous validations are ok then insert data on the DB
+            if (_dbRepository.AddNewClient(NewClient)>0)
+                MessageBox.Show($"The new Client {NewClient.ClientName} was properly added to the DB.");
+            else
+                MessageBox.Show($"Any new client was not added to the DB. Please check with the suport!");
         }        
+        private void AddNewOrder(object? parameter)
+        {
+            // Data validation
+            if (NewOrder == null)
+            {
+                MessageBox.Show($"Order data is missing");
+                return;
+            }
+            else if (!NewOrderEmptyFieldsValidation(NewOrder))
+            {
+                MessageBox.Show($"All Order fields are required.");
+                return;
+            }
+
+            // Format Data Validation (Optional)
+            // Max Length Validation (Optional)
+
+            // Client Id validation
+            else if (!FindClientByOrderId(NewOrder.CClient))
+            {
+                MessageBox.Show($"The order cannot be created because there is no client " +
+                                $"with the ID you entered as CClient.\n" +
+                                $"Pleas select an exising Client ID and try again.");
+                return;
+            }
+
+            // If all previous validations are ok then insert data on the DB
+            if (_dbRepository.AddNewOrder(NewOrder) > 0)
+                MessageBox.Show($"The new Order was properly added to the DB.");
+            else
+                MessageBox.Show($"Any new order was not added to the DB. Please check with the support!");
+        }
+        private void AddNewArticle(object? parameter)
+        {
+            // Data validation
+            if (NewArticle == null)
+            {
+                MessageBox.Show($"Article data is missing");
+                return;
+            }
+            else if (!NewArticlesEmptyFieldsValidation(NewArticle))
+            {
+                MessageBox.Show($"All Article fields are required.");
+                return;
+            }
+
+            // Format Data Validation (Optional)
+            // Max Length Validation (Optional)
+
+            // Duplicates validation
+            else if (FindArticleByName(NewArticle))
+            {
+                MessageBox.Show($"The inserted Article already exists on the DB.");
+                return;
+            }
+
+            // If all previous validations are ok then insert data on the DB
+            if (_dbRepository.AddNewArticle(NewArticle) > 0)
+                MessageBox.Show($"The new Article {NewArticle.ArticleName} was properly added to the DB.");
+            else
+                MessageBox.Show($"Any new article was not added to the DB. Please check with the support!");
+        }
+        #endregion
+        #region Update Methods
+
+        #endregion
+        #region Delete Methods
+
+        #endregion
+
     }
 }
